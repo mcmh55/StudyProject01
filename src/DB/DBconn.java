11889e2de10e17bd4d8207e585247568469e4fbc
김명호 <mcmh55@naver.com>
2016-07-11 19:08:13 +0900

    db 변경중

--- src/DB/DBconn.java
+++ src/DB/DBconn.java
@@ -1,6 +1,7 @@
 package DB;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -8,7 +9,7 @@ import javax.sql.DataSource;
 
 public class DBconn {
 
-	public Connection conn;
+	public static Connection conn;
 	
 	// 싱글톤 패턴: 단일 객체
 	private static DBconn instance = new DBconn();
@@ -17,8 +18,19 @@ public class DBconn {
 		return instance;
 	}
 	
-	public Connection getConnection() throws Exception {
+	public static Connection getConnection() throws Exception {
 		
+		/*
+		// 기본 DB 접속
+		conn = null;
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String id = "hr";
+		String pw = "hr";
+		conn = DriverManager.getConnection(url, id, pw);
+		*/
+		
+		// 커넥션 풀 처리 사용
 		conn = null;
 		Context initContext = new InitialContext();
 		Context envContext = (Context) initContext.lookup("java:/comp/env");
