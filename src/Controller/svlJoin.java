55160739cc4fe0fec0d4f158d4bb8430c155ad71
김명호 <mcmh55@naver.com>
2016-07-18 18:05:44 +0900

    fix - 별명 체크

--- src/Controller/svlJoin.java
+++ src/Controller/svlJoin.java
@@ -33,26 +33,29 @@ public class svlJoin extends HttpServlet {
 		
 		String url = "member/Join.jsp";
 		String inputId = request.getParameter("id");
+		String inputNick = request.getParameter("nick");
 		String inputEmail =  request.getParameter("email");
 		
 		MemberDAO mdao = MemberDAO.getInstance();
-		boolean resultId = mdao.checkId(inputId);		// 아이디 중복 체크
+		boolean resultId = mdao.checkId(inputId);			// 아이디 중복 체크
+		boolean resultNick = mdao.checkNick(inputNick);		// 별명 중복 체크
 		boolean resultEmail = mdao.checkEmail(inputEmail);	// 이메일 중복 체크
 		
 		if ( resultId ) {
 			request.setAttribute("msg", "이미 존재하는 회원입니다.");
+		} else if ( resultNick ) {
+			request.setAttribute("msg", "이미 존재하는 별명입니다.");
 		} else if ( resultEmail ) {
 			request.setAttribute("msg", "이미 존재하는 이메일입니다.");
 		} else {
 			
 			String pw = request.getParameter("pw");
-			String nick = request.getParameter("nick");
 			String age = request.getParameter("age");
 			
 			MemberDTO mdto = new MemberDTO();
 			mdto.setId(inputId);
 			mdto.setPw(pw);
-			mdto.setNick(nick);
+			mdto.setNick(inputNick);
 			mdto.setAge(Integer.parseInt(age));
 			mdto.setEmail(inputEmail);
 			
